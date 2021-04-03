1c8bd637198dc4e58067b97da33ad514d0338d38
Mitchell Schimmel <mschimme@oa-c5.msk.21-school.ru>
2021-04-03 19:14:54 +0300

    updatet our_dump.sh, now it is more flexy

--- my_dump.sh
+++ my_dump.sh
@@ -6,7 +6,7 @@
 #    By: mschimme <mschimme@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/27 14:20:07 by mschimme          #+#    #+#              #
-#    Updated: 2021/04/02 23:54:37 by mschimme         ###   ########.fr        #
+#    Updated: 2021/04/03 19:14:23 by mschimme         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,8 +20,10 @@
 
 champ="/tmp/champ1.cor /tmp/champ2.cor"
 my_cmd="vimdiff"
-cwr_one="../corewar"
-cwr_two="./corewar"
+# Обычно, это наш (ваш) корвар; указывать с ключом -d/-dump
+cwr_one="../corewar -dump"
+#Обычно это референсный корвар (Школьный / Бражник); указывать с ключом -d/-dump
+cwr_two="./corewar -d"
 
 if [[ $1 == [0-9]*\-[0-9]* ]]; then
 	min=$(echo $1 | cut -d '-' -f 1)
@@ -32,19 +34,19 @@ if [[ $1 == [0-9]*\-[0-9]* ]]; then
 		max=$buf
 	fi
 	while [ $min -lt $max ]; do
-		../corewar -dump $min $champ > ../logs/"$min"_our_log
-		./corewar_school -d $min $champ > ../logs/"$min"_sch_log
+		$cwr_one -dump $min $champ > ../logs/"$min"_our_log
+		$cwr_two -d $min $champ > ../logs/"$min"_sch_log
 		$my_cmd ../logs/"$min"_our_log ../logs/"$min"_sch_log
 		((min++))
 	done
-	../corewar -dump $min $champ > ../logs/"$min"_our_log
-	../corewar_school -d $min $champ > ../logs/"$min"_sch_log
+	$cwr_one -dump $min $champ > ../logs/"$min"_our_log
+	$cwr_two -d $min $champ > ../logs/"$min"_sch_log
 	$my_cmd ../logs/"$min"_our_log ../logs/"$min"_sch_log
 else
 		while [ ! -z $1 ]; do
 			if [[ $1 =~ ^[0-9]*$ ]]; then
-				../corewar -dump $1 $champ > ../logs/"$1"_our_log
-				../corewar_school -d $1 $champ > ../logs/"$1"_sch_log
+				$cwr_one -dump $1 $champ > ../logs/"$1"_our_log
+				$cwr_two -d $1 $champ > ../logs/"$1"_sch_log
 				$my_cmd ../logs/"$1"_our_log ../logs/"$1"_sch_log
 			fi
 			shift
